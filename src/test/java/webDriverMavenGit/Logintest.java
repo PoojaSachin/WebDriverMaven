fc87f81ea80f7d2e1c1336ece0550edb215c79c5
PoojaSachin <poojarandive17@gmail.comgit config --global user.name Poojagit config --global user.email poojarandive17@gmail.com>
2020-06-02 11:16:15 +0530

    initial commit

---
+++ src/test/java/webDriverMavenGit/Logintest.java
@@ -0,0 +1,31 @@
+package webDriverMavenGit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class Logintest {
+
+	public static WebDriver driver;
+	@BeforeSuite
+	public static void setup(){
+		
+		driver = new ChromeDriver();
+		
+	}
+		
+	
+	@Test
+	public static void login(){
+		driver.get("http://gmail.com");	
+	}
+
+	@AfterSuite
+	public static void teardown(){
+		
+		driver.quit();
+		
+	}
+}
